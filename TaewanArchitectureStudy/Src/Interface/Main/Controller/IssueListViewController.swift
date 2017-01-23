66b8895f0e1f781f788696029398599bd66ac19c
taewan0530 <kimtaewan@kimtaewan-ui-Mac-mini.local>
2017-01-23 14:01:17 +0900

    뿅뿅

--- TaewanArchitectureStudy/Src/Interface/Main/Controller/IssueListViewController.swift
+++ TaewanArchitectureStudy/Src/Interface/Main/Controller/IssueListViewController.swift
@@ -125,6 +125,11 @@ extension IssueListViewController {
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
         collectionView.reloadData()
     }
     
